f3c008e9aebeba3ee37ce36a8f8adf02d6b23647
ohisama3110 <186119741+ohisama3110@users.noreply.github.com>
2025-02-05 10:44:20 +0000

    デプロイ

--- db/migrate/20250125150146_add_dish_name_to_recipes.rb
+++ db/migrate/20250125150146_add_dish_name_to_recipes.rb
@@ -1,8 +1,5 @@
 class AddDishNameToRecipes < ActiveRecord::Migration[6.1]
   def change
     add_column :recipes, :dish_name, :string
-    add_column :recipes, :ingredient, :string
-    add_column :recipes, :quantity, :string
-    add_column :recipes, :process, :string
   end
 end
