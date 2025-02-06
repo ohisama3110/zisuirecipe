a22c6786bdfe4266b6d38382aad3e510831a29a7
ohisama3110 <186119741+ohisama3110@users.noreply.github.com>
2025-02-06 06:18:08 +0000

    1stデプロイ+テスト完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "seedの実行を開始"
+
+
+users = [
+  { name: 'Alice', email: 'alice@example.com', password: 'password' },
+  { name: 'Bob', email: 'bob@example.com', password: 'password' },
+]
+
+users.each do |user_data|
+  user = User.create(user_data)
+end
+
+recipes = [
+  { dish_name: 'オムライス', ingredient: '卵、ご飯、ケチャップ', servings: 2},
+  { dish_name: 'カレーライス', ingredient: 'カレールー、ご飯、具材', servings: 4},
+]
+
+recipes.each do |recipe_data|
+  user = User.find_by(name: 'Alice')
+  recipe_data[:user_id] = user.id
+  Recipe.create(recipe_data)
+end
